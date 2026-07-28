ccce12a31a1afdc48ff823ccb4a44235859989a9
rishabreddy0809 <rishabreddy0809@gmail.com>
2026-07-28 16:58:14 -0400

    Initial Commit

---
+++ Scholar/ScholarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarApp.swift
+//  Scholar
+//
+//  Created by Rishab Reddy Paili on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ScholarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
